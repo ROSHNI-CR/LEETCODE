2358356c13cab3cbada4ac1e3f33d2c5b65c719b
ROSHNI-CR <roshnikhanam59@gmail.com>
2026-06-12 16:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (60.74%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def xorOperation(self, n, start):
+        nums=[]
+        res=0
+        
+        for i in range(n):
+            nums.append(start+2*i)
+            
+            res=res ^ nums[i]
+        return res
+
+        
+        
